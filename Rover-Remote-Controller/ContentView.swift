ee5acab450f928ba12bedf9dbce2024aa86157df
Mathieu <mathieu.dubart@edu.gobelins.fr>
2024-11-04 16:20:51 +0100

    Initial Commit

---
+++ Rover-Remote-Controller/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Rover-Remote-Controller
+//
+//  Created by Mathieu DUBART on 04/11/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Rover-Remote-Controller/Rover_Remote_ControllerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rover_Remote_ControllerApp.swift
+//  Rover-Remote-Controller
+//
+//  Created by Mathieu DUBART on 04/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct Rover_Remote_ControllerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
